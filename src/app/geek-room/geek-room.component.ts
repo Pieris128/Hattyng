2e45003a4e94f8d747fb994960dd6c80bf569d70
Pieris128 <elpichichi2048@gmail.com>
2022-10-10 12:40:28 -0300

    Msg reading and sending logic done on Geek comp

--- src/app/geek-room/geek-room.component.ts
+++ src/app/geek-room/geek-room.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { onAuthStateChanged } from 'firebase/auth';
 import { Firebase } from '../firebase.service';
-import { onChildAdded, onChildRemoved, ref } from 'firebase/database';
+import {
+  endAt,
+  limitToLast,
+  onChildAdded,
+  onChildRemoved,
+  query,
+  ref,
+  onValue,
+  startAt,
+  QueryConstraint,
+} from 'firebase/database';
 
 @Component({
   selector: 'app-geek-room',
   templateUrl: './geek-room.component.html',
   styleUrls: ['./geek-room.component.scss'],
 })
-export class GeekRoomComponent implements OnInit, OnDestroy {
+export class GeekRoomComponent implements OnInit, OnDestroy, AfterViewInit {
+  // GENERAL USER DATA RETRIEVED FROM DB
   userData!: {
     displayName: string;
     username: string;
@@ -17,6 +28,8 @@ export class GeekRoomComponent implements OnInit, OnDestroy {
     age: string;
     profile_picture: string;
   };
+
+  // USERS ON DB OBJS
   user: { name: string; img: string } = {
     name: '',
     img: '',
@@ -29,7 +42,18 @@ export class GeekRoomComponent implements OnInit, OnDestroy {
   };
   usersNames!: string[];
 
+  // MSGS OBJs
+  msgs!: {
+    [id: string]: {
+      name: string;
+      msg: string;
+      side: string;
+    };
+  };
+  msgsIDs!: string[];
+
   constructor(private firebase: Firebase) {
+    // USERS ON ROOM READING
     onChildAdded(ref(this.firebase.database, 'rooms/geek/users'), () => {
       this.getRoomList();
     });
@@ -38,9 +62,21 @@ export class GeekRoomComponent implements OnInit, OnDestroy {
     });
   }
 
+  async submitMsg(msg: string, msgField: HTMLInputElement) {
+    let newMsg = await this.firebase.pushMsg(
+      'geek',
+      this.userData.displayName,
+      msg
+    );
+    msgField.value = '';
+    return newMsg;
+  }
+
   ngOnInit(): void {
     this.setUsersList();
   }
+
+  ngAfterViewInit(): void {}
 
   ngOnDestroy(): void {
     this.firebase.removeRoomUsersList('geek', this.user.name);
@@ -65,8 +101,39 @@ export class GeekRoomComponent implements OnInit, OnDestroy {
           this.user.img
         );
         this.getRoomList();
+        this.initChatRoom();
       }
-      console.log('auth changed');
+    });
+  }
+
+  // INIT LOGIC FOR SETTING CHAT OBJS ON REALTIME
+  initChatRoom() {
+    // MSG FROM DB READING LOGIC
+    let newMsgsQuery = query(
+      ref(this.firebase.database, 'rooms/geek/msgs'),
+      limitToLast(1)
+    );
+
+    onChildAdded(newMsgsQuery, (snapshot) => {
+      let msgObject: { name: string; msg: string; side: string } =
+        snapshot.toJSON() as {
+          name: string;
+          msg: string;
+          side: 'left';
+        };
+
+      if (msgObject.name === this.userData.displayName) {
+        msgObject.side = 'right';
+      }
+
+      if (this.msgs) {
+        this.msgs[snapshot.key!] = msgObject;
+      } else {
+        this.msgs = { [snapshot.key!]: msgObject };
+      }
+
+      this.msgsIDs = Object.keys(this.msgs);
+      this.msgsIDs.shift();
     });
   }
 }
