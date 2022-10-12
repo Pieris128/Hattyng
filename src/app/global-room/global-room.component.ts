ecc6337fee6528dcca81f4bf67b43814dcf50bf9
GoorezyEST <goorezyestudios@gmail.com>
2022-10-12 14:18:28 -0300

    Added functionality to the Global room component

--- src/app/global-room/global-room.component.ts
+++ src/app/global-room/global-room.component.ts
@@ -1,15 +1,194 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { onAuthStateChanged } from 'firebase/auth';
+import { Firebase } from '../firebase.service';
+import {
+  limitToLast,
+  off,
+  onChildAdded,
+  onChildRemoved,
+  onValue,
+  query,
+  ref,
+} from 'firebase/database';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-global-room',
   templateUrl: './global-room.component.html',
-  styleUrls: ['./global-room.component.scss']
+  styleUrls: ['./global-room.component.scss'],
 })
-export class GlobalRoomComponent implements OnInit {
+export class GlobalRoomComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChildren('msgDiv') msgDiv!: QueryList<ElementRef>;
+  focusSub!: Subscription;
+  // GENERAL USER DATA RETRIEVED FROM DB
+  userData!: {
+    displayName: string;
+    username: string;
+    description: string;
+    nacionality: string;
+    age: string;
+    profile_picture: string;
+  };
 
-  constructor() { }
+  // USERS ON DB OBJS
+  user: { name: string; img: string } = {
+    name: '',
+    img: '',
+  };
+  totalUsers!: {
+    [name: string]: {
+      name: string;
+      img: string;
+    };
+  };
+  usersNames!: string[];
 
-  ngOnInit(): void {
+  // MSGS OBJs
+  msgs!: {
+    [id: string]: {
+      name: string;
+      msg: string;
+      side: string;
+    };
+  };
+  msgsIDs!: string[];
+
+  //TextArea El
+  textArea!: HTMLTextAreaElement;
+
+  constructor(private firebase: Firebase) {
+    // USERS ON ROOM READING
+    onChildAdded(ref(this.firebase.database, 'rooms/global/users'), () => {
+      this.getRoomList();
+    });
+    onChildRemoved(ref(this.firebase.database, 'rooms/global/users'), () => {
+      this.getRoomList();
+    });
   }
 
+  async submitMsg(msg: string, msgField: HTMLTextAreaElement) {
+    if (msg === '') {
+      return;
+    }
+    let newMsg = await this.firebase.pushMsg(
+      'global',
+      this.userData.displayName,
+      msg
+    );
+    msgField.value = '';
+    return newMsg;
+  }
+
+  ngOnInit(): void {
+    this.setUsersList();
+  }
+
+  ngAfterViewInit(): void {
+    this.textArea = document.querySelector('.room__chat__enter__input')!;
+    this.focusSub = this.msgDiv.changes.subscribe(() => {
+      if (this.msgDiv && this.msgDiv.last) {
+        this.msgDiv.last.nativeElement.focus();
+        this.textArea.focus();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firebase.removeRoomUsersList('global', this.user.name);
+    this.focusSub.unsubscribe();
+    if (this.usersNames.length === 1) {
+      this.firebase.removeRoomMsgs('global');
+    }
+    off(
+      query(ref(this.firebase.database, 'rooms/global/msgs'), limitToLast(1))
+    );
+    off(ref(this.firebase.database, 'rooms/global/users'));
+  }
+
+  async getRoomList() {
+    this.totalUsers = await this.firebase.getRoomUsersList('global');
+
+    this.usersNames = Object.keys(this.totalUsers);
+  }
+
+  setUsersList() {
+    onAuthStateChanged(this.firebase.auth, async (user) => {
+      if (user) {
+        this.userData = await this.firebase.readUserData(user.displayName);
+        this.user.name = this.userData.displayName;
+        this.user.img = this.userData.profile_picture;
+
+        await this.firebase.writeRoomUsersList(
+          'global',
+          this.user.name,
+          this.user.img
+        );
+        this.getRoomList();
+        this.initChatRoom();
+      }
+    });
+  }
+
+  // INIT LOGIC FOR SETTING CHAT OBJS ON REALTIME
+  initChatRoom() {
+    let doShift = true;
+    onValue(
+      query(ref(this.firebase.database, 'rooms/global/msgs')),
+      (snapshot) => {
+        if (!snapshot.val()) {
+          doShift = false;
+        }
+      },
+      { onlyOnce: true }
+    );
+
+    onChildAdded(
+      query(ref(this.firebase.database, 'rooms/global/msgs'), limitToLast(1)),
+      (snapshot) => {
+        let msgObject: { name: string; msg: string; side: string } =
+          snapshot.toJSON() as {
+            name: string;
+            msg: string;
+            side: 'left';
+          };
+
+        if (msgObject.name === this.userData.displayName) {
+          msgObject.side = 'right';
+        } else {
+          msgObject.side = 'left';
+        }
+        if (this.msgs) {
+          this.msgs[snapshot.key!] = msgObject;
+        } else {
+          this.msgs = { [snapshot.key!]: msgObject };
+        }
+
+        this.msgsIDs = Object.keys(this.msgs);
+
+        if (doShift) {
+          this.msgsIDs.shift();
+        }
+      }
+    );
+  }
+
+  @HostListener('keypress', ['$event'])
+  onEnterPress($event: KeyboardEvent) {
+    if ($event.composedPath()[0] === this.textArea) {
+      if ($event.key === 'Enter' && !$event.shiftKey) {
+        $event.preventDefault();
+        let form = this.textArea.closest('form');
+        form?.requestSubmit();
+      }
+    }
+  }
 }
